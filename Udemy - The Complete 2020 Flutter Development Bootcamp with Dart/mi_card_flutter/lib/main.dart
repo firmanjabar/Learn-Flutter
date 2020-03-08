67284098b82c462c23805c1030d6b7dd8dd150e4
FirmanJabar <firmanabduljabar@gmail.com>
2020-03-09 01:54:56 +0800

    :sparkles: Custom Fonts

--- Udemy - The Complete 2020 Flutter Development Bootcamp with Dart/mi_card_flutter/lib/main.dart
+++ Udemy - The Complete 2020 Flutter Development Bootcamp with Dart/mi_card_flutter/lib/main.dart
@@ -10,34 +10,48 @@ class MiCard extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal[800],
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.green,
-                  ),
-                ],
+              CircleAvatar(
+                radius: 55.0,
+                backgroundColor: Colors.white,
+                backgroundImage: NetworkImage(
+                    'https://pbs.twimg.com/profile_images/1216180788909096960/xgByKsRu_400x400.jpg'),
               ),
               Container(
-                width: 100.0,
-                color: Colors.blue,
+                width: double.infinity,
+                height: 5.0,
+              ),
+              Text(
+                'Firman Abdul Jabar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35.0,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                  letterSpacing: 2.5,
+                  // fontFamily: 'SourceSansPro',
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                width: 120.0,
+                height: 0.5,
+                color: Colors.white,
               ),
             ],
           ),
