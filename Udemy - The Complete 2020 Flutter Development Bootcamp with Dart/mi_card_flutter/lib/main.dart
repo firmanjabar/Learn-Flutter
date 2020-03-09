48399e81fe67bbbb92388108d4ca97c76ef55b2a
FirmanJabar <firmanabduljabar@gmail.com>
2020-03-09 10:19:56 +0800

    :sparkles: MiCard Finish

--- Udemy - The Complete 2020 Flutter Development Bootcamp with Dart/mi_card_flutter/lib/main.dart
+++ Udemy - The Complete 2020 Flutter Development Bootcamp with Dart/mi_card_flutter/lib/main.dart
@@ -12,48 +12,108 @@ class MiCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal[800],
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 55.0,
-                backgroundColor: Colors.white,
-                backgroundImage: NetworkImage(
-                    'https://pbs.twimg.com/profile_images/1216180788909096960/xgByKsRu_400x400.jpg'),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 55.0,
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(
+                        'https://pbs.twimg.com/profile_images/1216180788909096960/xgByKsRu_400x400.jpg'),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    'Firman Abdul Jabar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35.0,
+                      fontFamily: 'Pacifico',
+                    ),
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      color: Colors.teal[100],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                      letterSpacing: 2.5,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                    width: 120.0,
+                    child: Divider(
+                      color: Colors.teal[100],
+                    ),
+                  ),
+                  Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone_android,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+62 899 2050 368',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.blueGrey[800],
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 0.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'firmanabduljabar@gmail.com',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'SourceSansPro',
+                          letterSpacing: 0.5,
+                          color: Colors.blueGrey[800],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 0.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.school,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'Lambung Mangkurat University',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'SourceSansPro',
+                          letterSpacing: 0.5,
+                          color: Colors.blueGrey[800],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                width: double.infinity,
-                height: 5.0,
-              ),
-              Text(
-                'Firman Abdul Jabar',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35.0,
-                  fontFamily: 'Pacifico',
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  color: Colors.teal[100],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15.0,
-                  letterSpacing: 2.5,
-                  // fontFamily: 'SourceSansPro',
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Container(
-                width: 120.0,
-                height: 0.5,
-                color: Colors.white,
-              ),
-            ],
+            ),
           ),
         ),
       ),
